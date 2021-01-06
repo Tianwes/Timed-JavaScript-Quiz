f01581ee62d244db86dac57d4a82e7e8140d460e
Tianwes <willnotsuffice@gmail.com>
2021-01-06 11:18:18 -0500

    Countdown timer now starts when start button is clicked and ends a zero

--- script.js
+++ script.js
@@ -2,69 +2,76 @@ startButton = document.getElementById("start-button");
 startButton.addEventListener("click", startGame);
 // answerButton = document.getElementsByClassName("btn");
 // answerButton.addEventListener("click",setInterval);
-
 homePage = document.getElementById("homePage-container");
 questionsStart = document.getElementById("questions-container");
 
 const timerEl = document.getElementById("timer");
+// timerEl.addEventListener("click", startTimer);
+// timerEl.addEventListener("click", startTimer);
 
 // one minute
 const startingTime = 1;
 // 60 seconds
 var time = startingTime * 60;
 
-setInterval(runCountDown, 1000);
+// var questions = [
+//     { q: "Which is not a data type?",
+//         choices: 
+//             1: "Boolean",
+//             2: "String",
+//             3: "Node",
+//             4: "Array",
+//             correctAnswer: "3"
+//     },
+//     { q: "Which symbol returns the division remainder?",
+//         choices: 
+//             1: "/", 
+//             2: "++", 
+//             3: "*", 
+//             4: "%",
+//             correctAnswer: "4"
+//     },
+//     { q: "How would you access \"giraffe\" in this array\:var zoo = [\"zebra\",\"hippo\",\"giraffe\"]; ?",
+//         choices: 
+//             1: "zoo[3]", 
+//             2: "zoo[2]", 
+//             3: "zoo[-3]", 
+//             4: "zoo[giraffe]",
+//             correctAnswer: "2"
+//     },
+//     { q: "Fixed values are called?",
+//         choices: 
+//             1: "Literals", 
+//             2: "Variables", 
+//             3: "Operators", 
+//             4: "Arrays",
+//             correctAnswer: "1"
+//     }
+// ];
 
-function runCountDown(){
-    const minutes = Math.floor(time / 60);
-    var seconds = time % 60;
-    if (seconds < 10){
-        seconds = "0" + seconds;
-    }else{
-        seconds;
-    }
-    timerEl.innerHTML = minutes + ":" + seconds;
-    time--;
-}
 
-var questions = [
-    { q: "Which is not a data type?",
-        choices: {
-            1: "Boolean",
-            2: "String",
-            3: "Node",
-            4: "Array"
-        },  correctAnswer: "3"
-    },
-    { q: "Which symbol returns the division remainder?",
-        choices: {
-            1: "/", 2: "++", 3: "*", 4: "%"
-        },
-        correctAnswer: "4"
-    },
-    { q: "How would you access \"giraffe\" in this array\:var zoo = [\"zebra\",\"hippo\",\"giraffe\"]; ?",
-        choices: {
-            1: "zoo[3]", 2: "zoo[2]", 3: "zoo[-3]", 4: "zoo[giraffe]"
-        },
-        correctAnswer: "2"
-    },
-    { q: "Fixed values are called?",
-        choices: {
-            1: "Literals", 2: "Variables", 3: "Operators", 4: "Arrays"
-        },
-        correctAnswer: "1"
-    }
-];
-
-// startClock();
-function startGame(){
+function startGame() {
     homePage.classList.add("hide");
     questionsStart.classList.remove("hide");
-    // timeDecrease();
-    // timer.innerHtml = 
-    
 
+      function startTimer() {
+        const minutes = Math.floor(time / 60);
+        var seconds = time % 60;
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        } if (time <= 0) {
+            return timerEl.innerHTML = "OH NO! Time's Up!";
+            // unhide replay button
+        } else {
+            seconds;
+        }
+        timerEl.innerHTML = minutes + ":" + seconds;
+        time--;
+
+    } 
+    setInterval(startTimer, 1000);
 }
+
 
 // function startQuestions(){
 
@@ -72,16 +79,16 @@ function startGame(){
 
  // setInterval(function(){ 
     //     for(var i = 0; i <= questions.length; i++){
-            
+
 
             // questions[0];  
 
         // i need to pass in an index into question and answer and go 
         // down array after button clicks
         // }
-        
+
     // }, 6000);
-    
+
 
 
 
