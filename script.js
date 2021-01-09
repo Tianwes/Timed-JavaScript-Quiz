5c707053462a36efe6ab7162b337b3d4a0cedec9
Tianwes <willnotsuffice@gmail.com>
2021-01-09 14:19:18 -0500

    Countdown starts when questions show, 1st q&a's is passed in successfully, need to loop thru rest of q's

--- script.js
+++ script.js
@@ -1,60 +1,74 @@
+// Homepage "start" button
 startButton = document.getElementById("start-button");
 startButton.addEventListener("click", startGame);
-// answerButton = document.getElementsByClassName("btn");
-// answerButton.addEventListener("click",setInterval);
+
 homePage = document.getElementById("homePage-container");
 questionsStart = document.getElementById("questions-container");
 
-const timerEl = document.getElementById("timer");
-// timerEl.addEventListener("click", startTimer);
-// timerEl.addEventListener("click", startTimer);
+// answerButton = document.getElementsByClassName("btn");
+// answerButton.addEventListener("click",setInterval);
 
+const timerEl = document.getElementById("timer");
 // one minute
 const startingTime = 1;
 // 60 seconds
 var time = startingTime * 60;
 
-// var questions = [
-//     { q: "Which is not a data type?",
-//         choices: 
-//             1: "Boolean",
-//             2: "String",
-//             3: "Node",
-//             4: "Array",
-//             correctAnswer: "3"
-//     },
-//     { q: "Which symbol returns the division remainder?",
-//         choices: 
-//             1: "/", 
-//             2: "++", 
-//             3: "*", 
-//             4: "%",
-//             correctAnswer: "4"
-//     },
-//     { q: "How would you access \"giraffe\" in this array\:var zoo = [\"zebra\",\"hippo\",\"giraffe\"]; ?",
-//         choices: 
-//             1: "zoo[3]", 
-//             2: "zoo[2]", 
-//             3: "zoo[-3]", 
-//             4: "zoo[giraffe]",
-//             correctAnswer: "2"
-//     },
-//     { q: "Fixed values are called?",
-//         choices: 
-//             1: "Literals", 
-//             2: "Variables", 
-//             3: "Operators", 
-//             4: "Arrays",
-//             correctAnswer: "1"
-//     }
-// ];
+var currentQuestion = [0];
 
+// Question and subsequent answer options
+var questionEl = document.getElementById("question");
+var option1 = document.getElementById("option-1");
+var option2 = document.getElementById("option-2");
+var option3 = document.getElementById("option-3");
+var option4 = document.getElementById("option-4");
+
+
+var questionsArr = [
+    {
+        q: "Which is not a data type?",
+        option1: "Boolean",
+        option2: "String",
+        option3: "Node",
+        option4: "Array",
+        correctAnswer: "3"
+    },
+    {
+        "question": "Which symbol returns the division remainder?",
+        "option1": "/",
+        "option2": "++",
+        "option3": "*",
+        "option4": "%",
+        "correctAnswer": "4"
+    },
+    {
+        "question": "How would you access \"giraffe\" in this array\:var zoo = [\"zebra\",\"hippo\",\"giraffe\"]; ?",
+        "option1": "zoo[3]",
+        "option2": "zoo[2]",
+        "option3": "zoo[-3]",
+        "option4": "zoo[giraffe]",
+        "correctAnswer": "2"
+    },
+    {
+        "question": "Fixed values are called?",
+        "option1": "Literals",
+        "option2": "Variables",
+        "option3": "Operators",
+        "option4": "Arrays",
+        "correctAnswer": "1"
+    }
+];
+// var allQuestions = questionsArr.length;
+var endOfQuestions = questionsArr.length;
 
 function startGame() {
-    homePage.classList.add("hide");
-    questionsStart.classList.remove("hide");
+    // homePage.classList.add("hide");
+    // questionsStart.classList.remove("hide");
 
-      function startTimer() {
+    function startTimer() {
+        insertQuestion();
+        homePage.classList.add("hide");
+        questionsStart.classList.remove("hide");
         const minutes = Math.floor(time / 60);
         var seconds = time % 60;
         if (seconds < 10) {
@@ -67,29 +81,20 @@ function startGame() {
         }
         timerEl.innerHTML = minutes + ":" + seconds;
         time--;
+        
+    // startGame();
 
-    } 
+    }
+    function insertQuestion(){
+    //     var q = questionsArr.q;
+    questionEl.innerText = questionsArr[0].q;
+        option1.textContent = questionsArr[0].option1;
+        option2.textContent = questionsArr[0].option2;
+        option3.textContent = questionsArr[0].option3;
+        option4.textContent = questionsArr[0].option4;
+    }
+    
     setInterval(startTimer, 1000);
-}
-
-
-// function startQuestions(){
-
-// }
-
- // setInterval(function(){ 
-    //     for(var i = 0; i <= questions.length; i++){
-
-
-            // questions[0];  
-
-        // i need to pass in an index into question and answer and go 
-        // down array after button clicks
-        // }
-
-    // }, 6000);
-
-
-
-
-
+    }
+    clearInterval();
+   
