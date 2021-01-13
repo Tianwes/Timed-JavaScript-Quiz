6c1d025d29b58afa77fc7da6bc01e51fd81e62e9
Tianwes <willnotsuffice@gmail.com>
2021-01-13 16:47:56 -0500

    Everything works, need to add readme and clean up, would like to add restart button

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ questionEl = document.getElementById("question");
 optionEl = Array.from(document.getElementsByClassName("option"));
 highScoreContainer = document.getElementById("your-score");
 initials = document.getElementById("initials-input");
-document.getElementById("start-button").addEventListener("click", startGame);
-document.getElementById("nextQ-button").addEventListener("click", loadNextQuestion);
 const timerEl = document.getElementById("timer");
-// Homepage "prevQ-button" button
-// document.getElementById("prevQ-button").addEventListener("click", loadPrevQuestion);
-// Question and subsequent answer options
+timesUpCont = document.getElementById("timesUp");
+// help bellow!
+scoreNumHere = document.getElementById("scoreNumGoesHere")
+scoreBoardCont = document.getElementById("scoreBoard");
+coderInt = document.getElementById("coderInt");
+userScore = document.getElementById("coderScore");
+highScoresList = document.getElementById("highScoresList");
+
 document.getElementById("option-1").addEventListener("click", function () { checkanswer("1") });
 document.getElementById("option-2").addEventListener("click", function () { checkanswer("2") });
 document.getElementById("option-3").addEventListener("click", function () { checkanswer("3") });
 document.getElementById("option-4").addEventListener("click", function () { checkanswer("4") });
-
+document.getElementById("start-button").addEventListener("click", startGame);
 document.getElementById("submit-initials-button").addEventListener("click", saveHighScore);
-
 
 // one minute
 const startingTime = 1;
@@ -35,15 +37,12 @@ var questionsArr = [
         question: "Which symbol returns the division remainder?", option1: "/", option2: "++", option3: "*", option4: "%", correctAnswer: "4"
     },
     {
-        question: "How would you access \"giraffe\" in this array? \: <br><br> var zoo = [\"zebra\",\"hippo\",\"giraffe\"]", option1: "zoo[3]", option2: "zoo[2]", option3: "zoo[-3]", option4: "zoo[giraffe]", correctAnswer: "2"
+        question: "How would you access \"giraffe\" in this array? \: <br> var zoo = [\"zebra\",\"hippo\",\"giraffe\"]", option1: "zoo[3]", option2: "zoo[2]", option3: "zoo[-3]", option4: "zoo[giraffe]", correctAnswer: "2"
     },
     {
         question: "Fixed values are called?", option1: "Literals", option2: "Variables", option3: "Operators", option4: "Arrays", correctAnswer: "1"
     }
 ];
-
-// var allQuestions = questionsArr.length;
-var endOfQuestions = questionsArr.length;
 
 function startGame() {
     timeInterval = setInterval(startTimer, 1000);
@@ -65,10 +64,11 @@ function startTimer() {
     }
 
     if (time <= 0) {
-        return timerEl.innerHTML = "OH NO! Time's Up!";
-        // unhide replay button
+        timerEl.innerHTML = "OH NO! Time's Up!";
+        quizStart.classList.add("hide");
+        timesUpCont.classList.remove("hide");
     }
-
+    
     timerEl.innerHTML = minutes + ":" + seconds;
     time--;
 
@@ -89,9 +89,9 @@ function checkanswer(value) {
         loadNextQuestion();
     }
     else {
-        loadNextQuestion();
         time = time - 9;
-        timerEl.innerHTML = time;
+        timerEl.innerHTML ="0:" + time;
+        loadNextQuestion();
     }
 }
 function loadNextQuestion() {
@@ -117,65 +117,52 @@ function loadNextQuestion() {
 function endGame() {
     highScoresPage();
 }
-// function loadPrevQuestion() {
-//     //var questionIndex = Math.floor(Math.random() * possibleQuestions.length);
-
-//     if (questionIndex > 0) {
-//         questionIndex -= 1;
-//     }
-
-//     currentQuestion = possibleQuestions[questionIndex];
-
-//     question.innerHTML = currentQuestion.question;
-
-//     answer.innerHTML = currentQuestion.correctAnswer;
-
-//     optionEl.forEach(optionEl => {
-//         var number = optionEl.dataset['number'];
-//         optionEl.innerText = currentQuestion["option" + number];
-
-//     });
-// };
 
 function highScoresPage() {
     quizStart.classList.add("hide");
     highScoreContainer.classList.remove("hide");
     clearInterval(timeInterval);
     var highScore = timerEl;
-    console.log(timerEl);
+    // console.log(timerEl);
+    scoreNumHere.innerText ="[" + time + "]";
 }
-// Got code below with help of tutor 
+const MAX_HIGHSCORES = 11;
+// Got some of code below with help of tutor 
 function saveHighScore() {
-    // number of right answers times time Left
-    // save initials of user
-    // create object id:score 
-    // stringify highscore 
-    var int = initials.value;
-
     var highScoresArr = [];
     highScoresArr = JSON.parse(localStorage.getItem("highScores")) || [];
-
     console.log(highScoresArr)
+    // save initials of user
+    var int = initials.value;
     var score = time;
-
+    // package 1 user initials with their score(time)
     scoreObj = {
         int,
         score
     }
     console.log(scoreObj);
+    // push each individual user's initials and score to end of array of highscores
     highScoresArr.push(scoreObj);
-    localStorage.clear();
+    // stringify highscore 
     localStorage.setItem("highScores", JSON.stringify(highScoresArr));
     console.log(localStorage);
     printHighScores(highScoresArr);
 
-}
-
 function printHighScores(highScoresArr) {
-    console.log(highScoresArr);
-    highScoresArr.sort(function (a, b) {
-        return b.score - a.score;
-    })
+    // move to scoreBoard container 
+    highScoreContainer.classList.add("hide");
+    scoreBoardCont.classList.remove("hide");
+
+    // console.log(highScoresArr);
+    highScoresArr.sort( (a, b) => b.score - a.score)
+    highScoresArr.splice(11);
+    
+    // for each object created, make it a list and append
+    highScoresArr.forEach(scoreObj => {
+        const listItem = document.createElement('li');
+        listItem.innerText = `${scoreObj.int}   -   ${scoreObj.score}`;
+        listItem.classList.add('highScoresList');
+        highScoresList.appendChild(listItem);
+       })
 }
-
-
+}
